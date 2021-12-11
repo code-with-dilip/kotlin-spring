8835ef43de64fc20fc71911199790b70de9e040a
Dilip Sundarraj <you@example.com>
2021-12-11 05:02:28 -0600

    commented out the jpa

--- course-catalog-service/build.gradle.kts
+++ course-catalog-service/build.gradle.kts
@@ -27,8 +27,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     //db
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("org.postgresql:postgresql")
+    //implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
